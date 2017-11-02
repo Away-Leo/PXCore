f871144950afa91f8420b99cb1415d8291560c91
liaoguowei <781720639@qq.com>
2017-11-02 14:36:35 +0800

    改进jsonAnalyzeUtil

--- PXCore/src/main/java/com/pingxundata/pxcore/utils/JsonAnalyzeUtil.java
+++ PXCore/src/main/java/com/pingxundata/pxcore/utils/JsonAnalyzeUtil.java
@@ -18,6 +18,7 @@ public class JsonAnalyzeUtil {
 
     public static JSONObject toJSONObject(String source) {
         if (ObjectHelper.isNotEmpty(source)) {
+            Log.v("toJSONObject======",source);
             return JSONObject.parseObject(source);
         } else {
             return null;
@@ -32,79 +33,83 @@ public class JsonAnalyzeUtil {
      * @return
      */
     public static RequestResult jsonStrToArray(String jsonStr, Class<?> clazz) {
-        if (ObjectHelper.isNotEmpty(jsonStr) && ObjectHelper.isNotEmpty(clazz)) {
-            RequestResult requestResult = new RequestResult();
-            JSONObject sourceJsonObject = toJSONObject(jsonStr);
-            if (ObjectHelper.isNotEmpty(sourceJsonObject.getBoolean("success"))) {
-                requestResult.setSuccess(sourceJsonObject.getBoolean("success"));
-            }
-            if (ObjectHelper.isNotEmpty(sourceJsonObject.getString("code"))) {
-                requestResult.setCode(sourceJsonObject.getString("code"));
-            }
-            if (ObjectHelper.isNotEmpty(sourceJsonObject.getString("message"))) {
-                requestResult.setMessage(sourceJsonObject.getString("message"));
-            }
-            try {
-                if (ObjectHelper.isNotEmpty(sourceJsonObject.getJSONArray("data"))) {
-                    try {
-                        requestResult.setResultList(parseJsonToBeanList(sourceJsonObject.getJSONArray("data").toJSONString(), clazz));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Log.e("将json字符串转换为集合出错", e.getMessage());
-                    }
+        try{
+            if (ObjectHelper.isNotEmpty(jsonStr) && ObjectHelper.isNotEmpty(clazz)) {
+                RequestResult requestResult = new RequestResult();
+                JSONObject sourceJsonObject = toJSONObject(jsonStr);
+                if (ObjectHelper.isNotEmpty(sourceJsonObject.getBoolean("success"))) {
+                    requestResult.setSuccess(sourceJsonObject.getBoolean("success"));
                 }
-            } catch (ClassCastException e) {
-                //如果为此结构则为集合类型
-                if (ObjectHelper.isNotEmpty(sourceJsonObject.get("data"))) {
-                    try {
-                        JSONObject jsonObject = sourceJsonObject.getJSONObject("data");
-                        if(jsonObject.containsKey("content")){
-                            if (ObjectHelper.isNotEmpty(jsonObject.getJSONArray("content"))) {
-                                try {
-                                    requestResult.setResultList(parseJsonToBeanList(jsonObject.getJSONArray("content").toJSONString(), clazz));
-                                } catch (Exception e1) {
-                                    e1.printStackTrace();
-                                    Log.e("将json字符串转换为集合出错", e1.getMessage());
-                                }
-                                if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("totalElements"))) {
-                                    requestResult.setTotalElements(jsonObject.getIntValue("totalElements"));
-                                }
-                                if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("totalPages"))) {
-                                    requestResult.setTotalPages(jsonObject.getIntValue("totalPages"));
-                                }
-                                if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("number"))) {
-                                    requestResult.setNumber(jsonObject.getIntValue("number"));
-                                }
-                                if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("size"))) {
-                                    requestResult.setSize(jsonObject.getIntValue("size"));
-                                }
-                            }
-                        }else{
-                            //此则为实体类型
-                            Object jsonObject1 = JSONObject.parseObject(sourceJsonObject.getJSONObject("data").toJSONString(), clazz);
-                            requestResult.setEntityResult(jsonObject1);
+                if (ObjectHelper.isNotEmpty(sourceJsonObject.getString("code"))) {
+                    requestResult.setCode(sourceJsonObject.getString("code"));
+                }
+                if (ObjectHelper.isNotEmpty(sourceJsonObject.getString("message"))) {
+                    requestResult.setMessage(sourceJsonObject.getString("message"));
+                }
+                try {
+                    if (ObjectHelper.isNotEmpty(sourceJsonObject.getJSONArray("data"))) {
+                        try {
+                            requestResult.setResultList(parseJsonToBeanList(sourceJsonObject.getJSONArray("data").toJSONString(), clazz));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.e("将json字符串转换为集合出错", e.getMessage());
                         }
-                    } catch (ClassCastException e1) {
-                        try{
-                            //此则为实体类型
-                            Object jsonObject1 = JSONObject.parseObject(sourceJsonObject.getJSONObject("data").toJSONString(), clazz);
-                            requestResult.setEntityResult(jsonObject1);
-                        }catch (ClassCastException e2){
+                    }
+                } catch (ClassCastException e) {
+                    //如果为此结构则为集合类型
+                    if (ObjectHelper.isNotEmpty(sourceJsonObject.get("data"))) {
+                        try {
+                            JSONObject jsonObject = sourceJsonObject.getJSONObject("data");
+                            if(jsonObject.containsKey("content")){
+                                if (ObjectHelper.isNotEmpty(jsonObject.getJSONArray("content"))) {
+                                    try {
+                                        requestResult.setResultList(parseJsonToBeanList(jsonObject.getJSONArray("content").toJSONString(), clazz));
+                                    } catch (Exception e1) {
+                                        e1.printStackTrace();
+                                        Log.e("将json字符串转换为集合出错", e1.getMessage());
+                                    }
+                                    if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("totalElements"))) {
+                                        requestResult.setTotalElements(jsonObject.getIntValue("totalElements"));
+                                    }
+                                    if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("totalPages"))) {
+                                        requestResult.setTotalPages(jsonObject.getIntValue("totalPages"));
+                                    }
+                                    if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("number"))) {
+                                        requestResult.setNumber(jsonObject.getIntValue("number"));
+                                    }
+                                    if (ObjectHelper.isNotEmpty(jsonObject.getIntValue("size"))) {
+                                        requestResult.setSize(jsonObject.getIntValue("size"));
+                                    }
+                                }
+                            }else{
+                                //此则为实体类型
+                                Object jsonObject1 = JSONObject.parseObject(sourceJsonObject.getJSONObject("data").toJSONString(), clazz);
+                                requestResult.setEntityResult(jsonObject1);
+                            }
+                        } catch (ClassCastException e1) {
                             try{
                                 //此则为实体类型
-                                Object jsonObject1=sourceJsonObject.get("data");
+                                Object jsonObject1 = JSONObject.parseObject(sourceJsonObject.getJSONObject("data").toJSONString(), clazz);
                                 requestResult.setEntityResult(jsonObject1);
-                            }catch (ClassCastException e3){
-                                RequestResult requestResult1=new RequestResult();
-                                requestResult1.setMessage(e3.getMessage());
-                                return requestResult1;
+                            }catch (ClassCastException e2){
+                                try{
+                                    //此则为实体类型
+                                    Object jsonObject1=sourceJsonObject.get("data");
+                                    requestResult.setEntityResult(jsonObject1);
+                                }catch (ClassCastException e3){
+                                    RequestResult requestResult1=new RequestResult();
+                                    requestResult1.setMessage(e3.getMessage());
+                                    return requestResult1;
+                                }
                             }
                         }
                     }
                 }
+                return requestResult;
+            } else {
+                return new RequestResult();
             }
-            return requestResult;
-        } else {
+        }catch (Exception e){
             return new RequestResult();
         }
     }
